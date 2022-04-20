aa046ad0dfbb7637d5487021fefbcf8badca583a
= <danielbarfa470@gmail.com>
2022-04-20 16:00:55 +0200

    improved the search bar

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginTestService } from '../services/loginTest.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -9,7 +9,15 @@ import { LoginTestService } from '../services/loginTest.service';
 export class NavComponent implements OnInit {
   logged: any = this.LoginTestService.isLogged();
   // checks if user is logged in
-  constructor(private LoginTestService: LoginTestService) {}
+  constructor(
+    private LoginTestService: LoginTestService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
+  test() {
+    console.log('test');
+
+    this.router.navigate(['/home']);
+  }
 }
